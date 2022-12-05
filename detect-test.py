c11a4eb00184047d11f35ffff11eb3c634f83398
Maengho-Lee <110338684+Maengho-Lee@users.noreply.github.com>
2022-12-05 22:58:08 +0900

    show text size modifi

--- detect-test.py
+++ detect-test.py
@@ -55,7 +55,7 @@ for i in range(len(boxes)):
         label = str(classes[class_ids[i]])
         color = colors[i]
         cv2.rectangle(img, (x, y), (x + w, y + h), color, 2)
-        cv2.putText(img, label, (x, y + 30), font, 3, color, 3)
+        cv2.putText(img, label, (x, y + 30), font, 2, color, 1)
 cv2.imshow("Image", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
